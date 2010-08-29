be405f57c571e675341075c72daf358442bc8617
no <no@newt.(none)>
2010-08-29 14:19:06 -0400

    some seq reading

--- src/ibycus/reader.clj
+++ src/ibycus/reader.clj
@@ -1,1 +1,24 @@
- (ns ibycus.reader)
+ (ns ibycus.reader
+   (:use [techne.bag]))
+
+(defn- add-to-vocab
+  [vocab word follower]
+  (let [bag (get vocab word {})]
+       (assoc vocab word (put bag follower))))
+
+(defn- into-vocab
+  [first-word]
+  (let [prev (ref first-word)]
+    (fn [vocab follower]
+      (dosync
+        (let [word (deref prev)]
+          (ref-set prev follower)
+          (add-to-vocab vocab word follower))))))
+
+(defn read-seq
+  [words vocab]
+  (reduce 
+    (into-vocab (first words)) {}
+    (rest words)))
+
+
--- test/ibycus/test/reader.clj
+++ test/ibycus/test/reader.clj
@@ -4,5 +4,8 @@
   )
 
 
-(deftest test-true
-  (is true))
+(deftest test-read-seq
+  (is (= {:a {:a 1}} (read-seq [:a :a] {})))
+  (is (= {:a {:a 2}} (read-seq [:a :a :a] {})))
+         
+         )
