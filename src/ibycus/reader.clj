baf6200615ebad1f0186a7db157540ec1152a876
no <no@newt.(none)>
2010-09-06 00:59:54 -0400

    inline

--- src/ibycus/reader.clj
+++ src/ibycus/reader.clj
@@ -6,19 +6,16 @@
   (let [bag (get vocab word {})]
        (assoc vocab word (put bag follower))))
 
-(defn- into-vocab
-  [first-word]
-  (let [prev (ref first-word)]
-    (fn [vocab follower]
-      (dosync
-        (let [word (deref prev)]
-          (ref-set prev follower)
-          (add-to-vocab vocab word follower))))))
-
 (defn words->vocab
   [words vocab]
-  (reduce 
-    (into-vocab (first words)) {}
-    (rest words)))
+  (let [prev (ref (first words))]
+    (reduce 
+      (fn [vocab follower]
+        (dosync
+          (let [word (deref prev)]
+            (ref-set prev follower)
+            (add-to-vocab vocab word follower))))
+      {}
+      (rest words))))
 
 
