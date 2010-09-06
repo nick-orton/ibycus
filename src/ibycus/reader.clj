ca986473c9fa128c3c75ce7cf490de0e69fc4b20
no <no@newt.(none)>
2010-09-06 02:45:23 -0400

    can read in from file

--- src/ibycus/reader.clj
+++ src/ibycus/reader.clj
@@ -1,21 +1,38 @@
  (ns ibycus.reader
-   (:use [techne.bag]))
+   (:use [techne.bag])
+   (:require [clojure.contrib.str-utils2 :as str]
+             [techne.files :as files]))
 
-(defn- add-to-vocab
+(defn- vocab+
   [vocab word follower]
   (let [bag (get vocab word {})]
        (assoc vocab word (put bag follower))))
 
+(defn ref-set? 
+  ;TODO: no
+  "ref-set and return old value"
+  [r value]
+  (let [old (deref r)]
+       (ref-set r value)
+       old))
+
 (defn words->vocab
   [words vocab]
-  (let [prev (ref (first words))]
     (reduce 
-      (fn [vocab follower]
-        (dosync
-          (let [word (deref prev)]
-            (ref-set prev follower)
-            (add-to-vocab vocab word follower))))
+      (let [prev (ref (first words))]
+        (fn [vocab follower]
+          (dosync 
+            (vocab+ vocab (ref-set? prev follower) follower))))
       {}
-      (rest words))))
+      (rest words)))
 
+(defn string->words
+  [s]
+  (str/split s #" "))  
 
+(defn file->vocab
+  [file]
+  (->
+    (files/file->string file)
+    (string->words)
+    (words->vocab {})))
