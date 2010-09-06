58bb6485fdc7af13eb766c715a5478a93396b624
no <no@newt.(none)>
2010-09-06 00:18:22 -0400

    better test around reading input

--- test/ibycus/test/reader.clj
+++ test/ibycus/test/reader.clj
@@ -1,11 +1,10 @@
 (ns ibycus.test.reader
   (:use [ibycus.reader] :reload-all)
-  (:use [clojure.test])
-  )
+  (:use [clojure.test]))
 
 
 (deftest test-read-seq
   (is (= {:a {:a 1}} (read-seq [:a :a] {})))
   (is (= {:a {:a 2}} (read-seq [:a :a :a] {})))
-         
-         )
+  (is (= {:a {:a 1 :b 1}} (read-seq [:a :a :b] {})))
+  (is (= {:a {:b 1} :b {:b 1}} (read-seq [:a :b :b] {}))))
