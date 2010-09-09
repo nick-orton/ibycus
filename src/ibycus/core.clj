0f9cf4f82116a7c1fb4eff943565089ad898bbf3
no <no@newt.(none)>
2010-09-09 08:34:30 -0400

    top level function

--- src/ibycus/core.clj
+++ src/ibycus/core.clj
@@ -1,1 +1,15 @@
 (ns ibycus.core)
+
+(defn next-word [prev vocab]
+  "a")
+
+(defn write-poem
+  [num-chars vocab]
+  (loop [i 0
+         prev ::START-TOKEN
+         poem []]
+    (let [word (next-word prev vocab)
+          poem+ (conj poem word)]
+      (if (< i num-chars)
+          (recur (inc i) word poem+)
+          poem+))))
