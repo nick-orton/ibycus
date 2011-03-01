f83322f9b395d3f005b40381e9f8fe2bffdab214
Nick Orton <orton.nick@gmail.com>
2011-03-01 12:02:30 -0500

    next-word

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.h2database/h2 "1.2.140"]
-                 [techne "0.5.0-SNAPSHOT"]])
+                 [techne "0.6.0"]])
--- src/ibycus/vocab.clj
+++ src/ibycus/vocab.clj
@@ -1,10 +1,12 @@
 (ns ibycus.vocab
-  (:require [techne.bag :as bag]))
+  (:require [techne.bag :as bag]
+            [clojure.contrib.math :as math]))
 
 (defprotocol Vocab
   (add [self word follower])
   (words [_])
-  (add-all [self words]))
+  (add-all [self words])
+  (next-word [self prev]))
 
 (defn- vocab-adding-acc [start]
   (let [prev (ref start)]
@@ -25,6 +27,13 @@
       self
       (rest words)))
   (words [_] (keys state))
+  (next-word
+    [self prev]
+    ;TODO not found case
+    (let [bag (get state prev)
+          pick (math/floor (rand (bag/total bag)))]
+          (nth (bag/->seq bag) pick)
+          ))
         
   Object
   (toString [_]
