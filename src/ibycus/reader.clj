55ecc848939468d5a7edd919881b6e5171233257
Nick Orton <orton.nick@gmail.com>
2011-02-28 21:59:33 -0500

    moving words->vocab to vocab

--- src/ibycus/reader.clj
+++ src/ibycus/reader.clj
@@ -3,28 +3,6 @@
              [techne.files :as files]
              [ibycus.vocab :as vocab]))
 
-(defn ref-get+set 
-  ;TODO: no
-  "ref-set and return old value"
-  [r value]
-  (let [old (deref r)]
-       (ref-set r value)
-       old))
-
-(defn words->vocab
-  [words]
-    (reduce 
-      (let [prev (ref (first words))]
-        (fn [vocab follower]
-          (dosync 
-            (vocab/add vocab (ref-get+set prev follower) follower))))
-      (vocab/create)
-      (rest words)))
-
-(defn words->vocab*
-  [words vocab ]
-  (print vocab))
-
 (defn string->words
   [s]
   (filter #(not (empty? %)) (str/split s #"[\s+\d*\(\)]")))  
@@ -34,4 +12,4 @@
   (->
     (files/file->string file)
     (string->words)
-    (words->vocab )))
+    (vocab/words->vocab )))
--- src/ibycus/vocab.clj
+++ src/ibycus/vocab.clj
@@ -2,7 +2,8 @@
   (:require [techne.bag :as bag]))
 
 (defprotocol Vocab
-  (add [vocab word follower]))
+  (add [vocab word follower])
+  (words->vocab [words]))
 
 (declare create)
 
@@ -12,11 +13,28 @@
    [vocab word follower]
    (let [bag (get state word (bag/create))]
         (create (assoc state word (bag/put bag follower)))))
-         
+        
   Object
   (toString [self]
-    (str state))
-  )
+    (str state)))
+
+(defn- ref-get+set 
+  ;TODO: no
+  "ref-set and return old value"
+  [r value]
+  (let [old (deref r)]
+       (ref-set r value)
+       old))
+
+(defn words->vocab
+   [words]
+   (reduce 
+     (let [prev (ref (first words))]
+          (fn [vocab follower]
+            (dosync 
+              (add vocab (ref-get+set prev follower) follower))))
+     (create)
+     (rest words)))
 
  
 (defn create 
