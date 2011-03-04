a7864e293ea27b4de80bb68737584898004df221
Nick Orton <orton.nick@gmail.com>
2011-03-04 15:24:12 -0500

    trying to launch the server as main

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject ibycus "1.0.0-SNAPSHOT"
+(defproject ibycus "0.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [com.h2database/h2 "1.2.140"]
-                 [techne "0.6.0"]])
+                 [techne "0.6.0"]]
+  :aot  [ibycus.server]
+  :main ibycus.server)
