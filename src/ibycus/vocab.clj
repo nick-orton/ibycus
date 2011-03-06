cbf2ab0b0eeb02536bd3129b898114a8ecf8cb38
Nick Orton <orton.nick@gmail.com>
2011-03-06 00:11:28 -0500

    making vocab delegate to a chain

--- src/ibycus/vocab.clj
+++ src/ibycus/vocab.clj
@@ -3,47 +3,43 @@
             [clojure.contrib.math :as math]))
 
 (defprotocol Vocab
-  (words [_])
   (add-all [self words])
-  (start-word [_])
-  (next-word [self prev]))
+  (start [self])
+  (next-word [self words]))
 
-(defprotocol Addable
-  (add [self key val]))
+(defprotocol Chain
+  (acc [self start])
+  (leaf [self prev]))
 
-(defn- vocab-adding-acc 
-  [start]
-  (let [prev (ref start)]
-       (fn [self follower]
-         (let [prev* (deref prev)]
-              (dosync 
-                (ref-set prev follower)
-                (add self prev* follower))))))
 
-(deftype BagVocab [state]
-  Addable
-    (add
-      [self word follower]
-      (let [bag (get state word (bag/create))]
-           (BagVocab. (assoc state word (bag/put bag follower)))))
-
+(deftype BagVocab [chain]
   Vocab
-    (add-all [self words]
-      (reduce (vocab-adding-acc (first words)) 
-        self
-        (rest words)))
-    (words [_] (keys state))
-    (start-word [_] (rand-nth (keys state)))
+    (add-all [_ words]
+      (BagVocab. (reduce (acc chain (first words)) 
+        chain
+        (rest words))))
+    (start [_] (rand-nth (keys chain)))
     (next-word
-      [self prev]
+      [_ words]
       ;TODO not found case
-      (let [bag (get state prev)]
-           (rand-nth (bag/->seq bag))))
-        
-  Object
-    (toString [_]
-      (str state)))
+      (let [bag (leaf chain words)]
+           (rand-nth (bag/->seq bag)))))
+
+
+(defrecord BagChain1 []
+  Chain
+    (leaf [self words]
+      (get self (last words)))
+    (acc 
+      [self start]
+      (let [prev (ref start)]
+           (fn [self follower]
+             (let [prev* (deref prev)]
+                  (dosync 
+                    (ref-set prev follower)
+                    (let [bag (get self prev* (bag/create))]
+                         (assoc self prev* (bag/put bag follower)))))))))
 
 (defn words->vocab
    [words]
-   (add-all (BagVocab. {}) words))
+   (add-all (BagVocab. (BagChain1.)) words))
--- src/ibycus/writer.clj
+++ src/ibycus/writer.clj
@@ -4,9 +4,9 @@
 (defn write-poem
   [vocab size]
   (loop [i 0
-         prev (vocab/start-word vocab)
+         prev (vocab/start vocab)
          poem [prev]]
-    (let [word (vocab/next-word vocab prev)
+    (let [word (vocab/next-word vocab poem)
           poem* (conj poem word)]
          (if (< i size)
            (recur (inc i) word poem*)
