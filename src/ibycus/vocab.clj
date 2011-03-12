93a05452547993f228d57af3b938fc02575f81b7
Nick Orton <nick@orton.biz>
2011-03-12 18:25:10 -0500

    simplifying vocab

--- src/ibycus/vocab.clj
+++ src/ibycus/vocab.clj
@@ -8,75 +8,35 @@
   (words [self])
   (next-word [self words]))
 
-(defprotocol Chain
-  (acc [self start])
-  (add [self ks v])
-  (start-chain [chain])
-  (leaf [self prev]))
+(defn acc 
+  [initials]
+  (let [ks (ref initials)]
+       (fn [bag-map v]
+         (let [ks* (deref ks)
+               bag (get bag-map ks* (bag/create))]
+              (dosync 
+                (ref-set ks (conj (vec (rest ks*)) v))
+                (assoc bag-map ks* (bag/put bag v)))))))
 
-
-(deftype BagVocab [chain]
+(deftype BagVocab [bag-map size]
   Vocab
     (add-all [_ words]
-      (BagVocab. (reduce (acc chain words) 
-        chain
-        (rest words))))
-    (start [_] (start-chain chain))
-    (words [_] (vals chain))
+      (let [init (take size words)
+            rst (take 5 (drop size words))]
+      (BagVocab. 
+        (reduce 
+          (acc (take size words)) 
+          bag-map
+          (drop size words))
+        size)))
+    (start [_] (rand-nth (keys bag-map)))
+    (words [_] (vals bag-map))
     (next-word
       [_ words]
       ;TODO not found case
-      (let [bag (leaf chain (take-last (:size chain) words))]
+      (let [bag (get bag-map (take-last size words))]
            (rand-nth (bag/->seq bag)))))
-
-
-(defrecord BagChain1 []
-  Chain
-    (start-chain [self] [(rand-nth (keys self))])
-    (leaf [self words]
-      (get self (last words)))
-    (add [self ks v] 
-         (let [k (first ks)
-               bag (get self k (bag/create))]
-              (assoc self k (bag/put bag v))))
-    (acc 
-      [self words]
-      (let [prev (ref (first words))]
-           (fn [self follower]
-             (let [prev* (deref prev)]
-                  (dosync 
-                    (ref-set prev follower)
-                    (add self '(prev*) follower)))))))
-
-(defn make-bc1 [] (BagChain1.))
-
-(defrecord MultiChain [size create-link-fun]
-  Chain
-    (start-chain [self] 
-      (let [link (rand-nth (keys self))]
-           (vec (cons link (start-chain (get self link))) )))
-    (leaf [self words]
-      (let [link (get self (first words))]
-           (leaf link (rest words))))
-    (add [self ks v]
-         (let [k (first ks)
-               chain (get self k (create-link-fun))]
-               (assoc self k (add chain (rest ks) v))))
-    (acc 
-      [self keys]
-      (let [ks (ref (take size keys))]
-           (fn [self follower]
-             (let [ks* (deref ks)]
-                  (dosync 
-                    (ref-set ks (conj (vec (rest ks*)) follower))
-                    (add self ks* follower)))))))
-
-(defn create-link-fun-builder [size]
-  (if (= 1 size)
-      (fn [] (BagChain1.))
-      (fn [] (MultiChain. size (create-link-fun-builder (- size 1))))))
 
 (defn words->vocab
    [words]
-   (add-all (BagVocab. (MultiChain. 2 make-bc1)) words))
-   ;(add-all (BagVocab. ((create-link-fun-builder 2))) words))
+   (add-all (BagVocab. {} 3) words))
