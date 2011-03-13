0299f20f913190646da2511147b36fb37062b923
Nick Orton <nick@orton.biz>
2011-03-13 15:24:40 -0400

    lowercase and filtering out ll

--- src/ibycus/reader.clj
+++ src/ibycus/reader.clj
@@ -1,12 +1,16 @@
  (ns ibycus.reader
-   (:require [clojure.contrib.str-utils2 :as str]
+   (:require [clojure.contrib.str-utils2 :as str-utils]
              [techne.files :as files]
+             [clojure.contrib.string :as contrib-str]
              [ibycus.vocab :as vocab]))
 
 (defn string->words
   [s]
-  ;TODO toLowerCase
-  (filter #(not (empty? %)) (str/split s #"[\s+\d*\(\),(--):\"\]\[\.*;?'!]")))  
+  (->> 
+    (str-utils/split s #"[\s+\d*\(\),(--):\"\]\[\.*;?'!]")
+    (filter #(not (empty? %)))
+    (filter #(nil? (#{"ll"} %)))
+    (map contrib-str/lower-case)))
 
 (defn file->vocab
   [file]
