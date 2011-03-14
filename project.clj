12b88b3ba48ad7b5012d471746edece2f6e7eda8
Nick Orton <orton.nick@gmail.com>
2011-03-14 12:19:23 -0400

    upgrading techne

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [techne "0.6.0"]])
+                 [techne "0.6.1"]])
