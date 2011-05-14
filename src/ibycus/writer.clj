be6d5fc66cebcbdc1ddf0e3335d5d3aad6a343ba
Nick Orton <orton.nick@gmail.com>
2011-05-14 14:04:47 -0400

    cleanup ns in writer

--- src/ibycus/writer.clj
+++ src/ibycus/writer.clj
@@ -1,30 +1,31 @@
 (ns ibycus.writer
   (:gen-class)
-  (:require  [clojure.contrib.str-utils2 :as str-utils2]
-             [ibycus.reader :as reader]
-             [ibycus.vocab :as vocab]))
+  (:use [clojure.contrib.str-utils2 :only [capitalize]])
+  (:use [ibycus.reader :only [v]])
+  (:use [ibycus.vocab :only [start next-word]]))
 
+
+;TODO no poem should start with punctuation
 (def puncts #{"." "?" "!"})
 
 (defn- vocab->poem-word-list
   [vocab size]
   (loop [i 0
-         poem (vocab/start vocab)]
-    (let [word (vocab/next-word vocab poem)
+         poem (start vocab)]
+    (let [word (next-word vocab poem)
           poem* (conj poem word)]
          (if (< i size)
-           (recur (inc i) poem*)
-           poem*))))
+             (recur (inc i) poem*)
+             poem*))))
 
 (defn- vocab->sentence 
   [vocab]
-  (loop [poem (vocab/start vocab)]
-    (let [word (vocab/next-word vocab poem)
+  (loop [poem (start vocab)]
+    (let [word (next-word vocab poem)
           poem* (conj poem word)]
          (if (puncts word)
            (conj (vec (butlast poem)) (str (last poem) word))
            (recur poem*)))))
- 
 
 (defn- attach-punctuation-marks-to-the-word-before
   [words ]
@@ -36,22 +37,28 @@
      (let [next (first others)
            after (rest others)]
           (if (puncts next)
-            (if (empty? after)
-              (conj out (str current next))
-              (recur (conj out (str current next)) (str-utils2/capitalize (first after)) (rest after)))
-            (recur (conj out current) next after)))))) 
+              (if (empty? after)
+                  (conj out (str current next))
+                  (recur (conj out (str current next)) 
+                         (capitalize (first after)) 
+                         (rest after)))
+              (recur (conj out current) next after)))))) 
 
 (defn- words->string
   [words]
   (->>
     (interpose " " words)
     (apply str)
-    (str-utils2/capitalize)))
+    (capitalize)))
 
+;TODO move write-poem and write-sentance into a Protocol Poet.
+;TODO create a poet that takes a vocabulary
+;move reader's v fun into a seperate file that creates the vocabulary and poet
+;  and does the write method
 (defn- write-poem
   [size]
   (->>
-    (vocab->poem-word-list reader/v size)
+    (vocab->poem-word-list v size)
     (attach-punctuation-marks-to-the-word-before)
     (filter #(not (= "." %)))
     (words->string)))
@@ -59,7 +66,7 @@
 (defn- write-sentence
   []
   (->>
-    (vocab->sentence reader/v)
+    (vocab->sentence v)
     (words->string)))
 
 (defn write
